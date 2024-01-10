3c9b2c63345830a3125c4c95c4dc77ea2ff9c05e
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-01-10 18:53:01 +1300

    add ps module

---
+++ colorscript.psm1
@@ -0,0 +1,58 @@
+function Show-ColorScript {
+    <#
+    .SYNOPSIS
+        Prints a random color script to the console.
+    .DESCRIPTION
+        Prints a random color script to the console. Use the -Name parameter to print a specific script. Use the -List parameter to list all available scripts.
+    .PARAMETER Name
+        The name of the script to print.
+    .PARAMETER List
+        List all available scripts.
+    .EXAMPLE
+        Show-ColorScript
+        Prints a random script.
+    .EXAMPLE
+        Show-ColorScript -Name "elfman"
+        Prints the elfman script.
+    .EXAMPLE
+        Show-ColorScript -List
+        Prints a list of all available scripts.
+    #>
+    param(
+        [Parameter(Mandatory = $false)]
+        [string]$Name,
+        [Parameter(Mandatory = $false)]
+        [switch]$List
+    )
+
+    if ($List) {
+        # List all scripts
+        $scripts = Get-ChildItem -Path "$PSScriptRoot/colorscripts" -Filter *.ps1
+        $scripts | ForEach-Object {
+            Write-Host $_.BaseName
+        }
+
+        return
+    }
+
+    if ($Name) {
+        # Get script by name
+        $script = Get-ChildItem -Path "$PSScriptRoot/colorscripts" -Filter "$Name.ps1"
+        if ($script) {
+            # Run script
+            & $script.FullName
+        } else {
+            Write-Error "Script '$Name' not found"
+        }
+
+        return
+    }
+
+    $scripts = Get-ChildItem -Path "$PSScriptRoot/colorscripts" -Filter *.ps1
+
+    # Get random script
+    $script = $scripts | Get-Random
+
+    # Run script
+    & $script.FullName
+}
